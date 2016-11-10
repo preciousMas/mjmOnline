e56ad5ab0db72debdf9217028e8cf3ec59a5d379
preciousMas <Precious.masipa@gmail.com>
2016-11-10 21:10:52 +0200

    Add files via upload

---
+++ AutoIndex.conf.php
@@ -0,0 +1,31 @@
+<?php
+
+
+$base_dir = 'folder_vault/';
+$icon_path = 'index_icons/winxp';
+$stylesheet = 'stylesheet.css';
+
+
+/*  Optional settings:  */
+$sub_folder_access = true;
+$index = '';
+$hidden_files = array(
+	"$log_file",
+	"$links_file",
+	"$description_file",
+	"$banned_list",
+	"$stylesheet",
+	"$download_count",
+	"index_icons",
+	"languages",
+	"*.php",
+	".ht*");
+$enable_searching = true;
+$show_dir_size = true;
+$folder_expansion = false;
+$header = '';
+$path_to_language_files = './languages/';
+$lang = 'en';
+$select_language = false;
+
+?>
---
+++ adminPanel.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+#header {
+    background:#00a2d1;
+ color:#f9f9f9;
+    text-align:center;
+    padding:5px;
+}
+#nav {
+    line-height:30px;
+    background-color:#eeeeee;
+    height:300px;
+    width:200px;
+    float:left;
+    padding:5px;	      
+}
+#section {
+    
+    background:#f9f9f9;
+ font-family:"Courier New", Courier, monospace;
+ height:300px;
+    width:976px;
+    float:left;
+    padding:10px;	 	 
+}
+#footer {
+    background-color:black;
+    color:white;
+    clear:both;
+    text-align:center;
+   padding:5px;	 	 
+}
+</style>
+</head>
+<body>
+
+<div id="header">
+<h1>MJM Online Admin Panel</h1>
+</div>
+
+<div id="nav">
+<a href="backup_database.php">Backup database</a><br>
+<a href="backup_Directory.php">Backup Directory</a><br>
+<a href="Database.php">Manage Database</a><br>
+<a href="Directory.php">Manage Directory</a><br>
+</div>
+
+<div id="section">
+
+<div id="footer">
+Copyright © created by Nthabiseng Masipa
+</div>
+
+</body>
+</html>
+
